a8eca2c13d9265949b40ccfe8f8e35e7e1fd69a8
Tiago Inovatech Developer <tiagolemespalhano@gmail.com>
2022-02-09 19:02:49 -0300

    Add files via upload

--- whats.php
+++ whats.php
@@ -1,3 +1,3 @@
-<a id="whats" href="https://web.whatsapp.com/send?phone=5554996779018" target="_blank">
+<a id="whats" class="whats pulandinho" href="https://web.whatsapp.com/send?phone=5554996779018" target="_blank">
   <i class="fa fa-whatsapp icone-whats"></i>
 </a>
