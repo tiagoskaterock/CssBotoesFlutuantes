79a8a421b657a4ac2b1db8574ce903ad95a7c2c0
Tiago Inovatech Developer <tiagolemespalhano@gmail.com>
2022-02-09 17:04:46 -0300

    Be happy!

---
+++ whats.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <a 
+    id="whats" 
+    href="https://api.whatsapp.com/send?phone=5551xxxxxxxxx" 
+    target="_blank"
+   >
+  <i class="fa fa-whatsapp icone-whats"></i>
+</a>
