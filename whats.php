46b2eeba96d8437a71f5277b027e27f20dbad9bc
Jimmy Cupcakes <tiagolemespalhano@gmail.com>
2022-02-21 17:36:17 -0300

    Ícone mudado para usar font awesome 6

--- whats.php
+++ whats.php
@@ -1,3 +1,3 @@
-<a id="whats" class="whats pulandinho" href="https://web.whatsapp.com/send?phone=5554996779018" target="_blank">
-  <i class="fa fa-whatsapp icone-whats"></i>
+<a id="whats" class="whats pulandinho" href="https://web.whatsapp.com/send?phone=5554992233830" target="_blank">
+  <i class="fa-brands fa-whatsapp icone-whats"></i>
 </a>
